66ef375418e3b1cc7a31dd7b3741fbab963a3f95
Slogan101 <newson190@gmail.com>
2022-09-19 14:26:36 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,14 @@
 
 /**
 *_strlen - returns the length of a string
-*@s: string
-*Return: returns length as integer
+* @s: string
+*Return: returns length as integer;
 */
 
 int _strlen(char *s)
 {
 	int len = 0;
-	while (*(s * len) != '\0')
+	while (*(s + len) != '\0')
 		len++;
 
 	return (len);
